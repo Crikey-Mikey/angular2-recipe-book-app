85ff4644e2c937967b62d093a9051970a031f977
Michael Lewis <Michael Lewis>
2016-12-12 20:32:56 +1100

    Complete form for new/edit recipes

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { RecipeService } from '../recipe.service';
 import { Subscription } from 'rxjs/Rx';
 import { 
@@ -23,7 +23,8 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
 
   constructor( private route: ActivatedRoute, 
   			   		 private recipeService: RecipeService,
-  			   		 private formBuilder: FormBuilder) { }
+  			   		 private formBuilder: FormBuilder,
+               private router: Router) { }
 
   ngOnInit() {
   	this.subscription = this.route.params.subscribe(
@@ -36,15 +37,51 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
   				this.isNew = true;
   				this.recipe = null;
   			}
+        this.initForm();
   		}
   	);
+  }
+
+  onSubmit() {
+    const newRecipe = this.recipeForm.value;
+    if (this.isNew) {
+      this.recipeService.addRecipe(newRecipe);
+    } else {
+      this.recipeService.editRecipe(this.recipe, newRecipe);
+    }
+    this.navigateBack();
+  }
+
+  onCancel() {
+    this.navigateBack();
+  }
+
+  onAddItem(name: string, amount: string) {
+    (<FormArray>this.recipeForm.controls['ingredients']).push(
+      new FormGroup({
+        name: new FormControl(name, Validators.required),
+        amount: new FormControl(amount, [
+          Validators.required,
+          Validators.pattern("\\d+")
+          ])
+      })
+    );
+
+  }
+
+  onRemoveItem(index: number) {
+    (<FormArray>this.recipeForm.controls['ingredients']).removeAt(index);
   }
 
   ngOnDestroy() {
   	this.subscription.unsubscribe();
   }
 
-  private initForm(isNew: boolean) {
+  private navigateBack() {
+    this.router.navigate(['../']);
+  }
+
+  private initForm() {
   	let recipeName = '';
   	let recipeImageUrl = '';
   	let recipeContent = '';
