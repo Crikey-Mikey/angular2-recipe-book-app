af03ab1d288e5ac78cc2a74b0dd532d6f289f71b
Michael Lewis <Michael Lewis>
2016-12-14 23:38:10 +1100

    refactor shopping-list as a module

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe';
 import { Ingredient } from '../shared';
+import { Headers, Http, Response } from '@angular/http';
+import 'rxjs/Rx';
 
 @Injectable()
 export class RecipeService {
+
+  recipesChanged = new EventEmitter<Recipe[]>();
 
 	private recipes: Recipe[] = [
     new Recipe('burger','tasty', 'https://s-media-cache-ak0.pinimg.com/564x/ec/6a/be/ec6abe1aa414e6cdc9e379494121b78e.jpg', [
@@ -13,7 +17,7 @@ export class RecipeService {
     new Recipe('salad','ok', 'http://pulsefitness.com.au/wordpress/wp-content/uploads/2014/11/salad.png', [])
   ];
 
-  constructor() { }
+  constructor(private http: Http) { }
 
   getRecipes() {
   	return this.recipes;
@@ -33,6 +37,25 @@ export class RecipeService {
 
   editRecipe(oldRecipe: Recipe, newRecipe: Recipe) {
     this.recipes[this.recipes.indexOf(oldRecipe)] = newRecipe;
+  }
+
+  storeData() {
+    const body = JSON.stringify(this.recipes);
+    const headers = new Headers({
+      'Content-Type': 'application/json'
+    });
+    return this.http.put('https://recipebook-390a1.firebaseio.com/recipes.json', body, { headers: headers });
+  }
+
+  fetchData () {
+    return this.http.get('https://recipebook-390a1.firebaseio.com/recipes.json')
+    .map( (response: Response) => response.json() )
+    .subscribe(
+      (data: Recipe[]) => {
+        this.recipes = data;
+        this.recipesChanged.emit(this.recipes);
+      }
+    );
   }
 
 }
